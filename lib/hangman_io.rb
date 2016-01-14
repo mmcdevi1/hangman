85e8a15a83ce3e3e5d2fd81826877d3e59952791
Andy <andy@wi.mit.edu>
2016-01-14 16:55:31 -0500

    Multiline strings and #{} string interpolation.
    http://ruby-doc.org/docs/ruby-doc-bundle/Manual/man-1.4/syntax.html#string
    http://ruby-doc.org/docs/ruby-doc-bundle/Manual/man-1.4/syntax.html#here_doc

--- lib/hangman_io.rb
+++ lib/hangman_io.rb
@@ -6,9 +6,11 @@ module Hangman
     end
 
     def welcome_message
-      puts
-      puts "Welcome to Hangman!"
-      puts
+      puts <<-MSG
+      
+      Welcome to Hangman!
+      
+      MSG
     end
 
     def enter_guess
@@ -17,26 +19,27 @@ module Hangman
 
     def results_for(player, correct_letters)
       system_clear
-      puts
-      puts
-      puts "Correct Guess: "
-      print player.correct_guesses.join(', ')
-      puts
-      puts
-      puts "Incorrect Guess: "
-      print player.incorrect_guesses.join(', ')
-      puts
-      puts
-      puts "Hangman: "
-      print correct_letters.join
-      puts
-      puts
+      puts <<-MSG
+      
+      
+      Correct Guess: #{player.correct_guesses.join(', ')}"
+      
+      
+      Incorrect Guess: #{player.incorrect_guesses.join(', ')}
+      
+      
+      Hangman: #{correct_letters.join}
+      
+      
+      MSG
     end
 
     def quit
-      puts
-      puts 'You guessed the correct word! Hooray!!'
-      puts
+      puts <<-MSG
+      
+      You guessed the correct word! Hooray!!
+      
+      MSG
     end
 
     def system_clear
